e0b15e38b313f2afe5d4638884274d82288dcdd4
Utsav-001 <138020136+Utsav-001@users.noreply.github.com>
2025-08-04 23:11:00 +0530

    connection update

--- conn.php
+++ conn.php
@@ -1,9 +1,18 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'allure');
+// $conn = new mysqli('localhost', 'root', '', 'allure');
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+
+$conn = new mysqli('sql12.freesqldatabase.com', 'sql12793279', 'SaTNhp1yGw', 'sql12793279');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
+
+?>
 
 
   
