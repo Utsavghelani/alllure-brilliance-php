5bb8d273dbbf450e5c00cdac250a422eda05e2f0
Utsav-001 <138020136+Utsav-001@users.noreply.github.com>
2025-08-07 21:25:18 +0530

    bug fix

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
     <meta charset="utf-8">
-    <title>All ure brilliance</title>
+    <title>Allure brilliance</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
