2590b4cef0f2aa3f54d6ad4df55c44c723d3fed9
Utsav-001 <138020136+Utsav-001@users.noreply.github.com>
2025-08-21 22:40:55 +0530

    bug fix

--- conn.php
+++ conn.php
@@ -5,12 +5,15 @@
 // }
 
 
-$conn = new mysqli('sql12.freesqldatabase.com', 'sql12793279', 'SaTNhp1yGw', 'sql12793279');
+// $conn = new mysqli('sql12.freesqldatabase.com', 'sql12793279', 'SaTNhp1yGw', 'sql12793279');
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+$conn = new mysqli("localhost","greenlyfegrow","Radhe@2812","greenlyfe");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-
 
 ?>
 
